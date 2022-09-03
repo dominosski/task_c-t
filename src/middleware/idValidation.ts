0c79cd91c638ca6429808507d496c781bcd99cb6
dominosski <domix852@gmail.com>
2022-09-03 15:55:45 +0200

    fix: fixed validation for id parameter

--- src/middleware/idValidation.ts
+++ src/middleware/idValidation.ts
@@ -1,7 +1,7 @@
 export const validateId = (req:any, res:any, next:any) => {
     try{
         const id = req.body.id;
-        if(!id || id <= 10 || id >= 20)
+        if(!id || id < 10 || id > 20)
             throw new Error()
         else{
             next()
